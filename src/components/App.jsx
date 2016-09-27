b895993624d321d0dd45078382732bbc28b53428
tim_coomer <thcoomer@hotmail.com>
2016-09-28 07:13:49 +1300

    initial bundle set up with bugs

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+})
